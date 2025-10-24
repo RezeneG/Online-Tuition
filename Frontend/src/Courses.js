635d39389ac4fb6fb2c57a67787b564ea84f07ea
Rezene Ghebrehiwot <supsis@hotmail.com>
2025-10-24 08:05:11 +0100

    Create Courses.js

---
+++ Frontend/src/Courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Courses = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Courses Page</h1>
+      <p>Coming soon - Browse all available courses</p>
+    </div>
+  );
+};
+
+export default Courses;
